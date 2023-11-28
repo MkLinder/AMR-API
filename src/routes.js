67bc7099347596ed4a3c5e73eb33ada72446e4fb
Rafael <rafa_rodrigues01@yahoo.com>
2023-11-28 12:06:32 -0300

    feat: Add route for user login with imported loginSchema for user information validation

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 const { Router } = require('express');
 const validateBodyRequisition = require('./middlewares/validateBodyRequisition');
 const schemaUsers = require('./schemas/registerUsers');
-const { registerUser } = require('./controllers/users');
+const { registerUser, login } = require('./controllers/users');
+const loginSchema = require('./schemas/login');
 const route = Router();
 
 
 route.post('/usuario', validateBodyRequisition(schemaUsers), registerUser);
+route.post('/login', validateBodyRequisition(loginSchema), login)
 
 
 module.exports = route;
