57ded488e5c4d0a114243ab014be5a70a83104a1
Mike Linder <94409465+MkLinder@users.noreply.github.com>
2023-12-02 15:43:26 -0300

    Created editCustomerData

--- src/controllers/customers.js
+++ src/controllers/customers.js
@@ -1,18 +1,49 @@
 const database = require("../connection");
+const nameFormatter = require("../utils/nameFormatter");
+const cpfFormatter = require("../utils/cpfFormatter");
 
 const registerCustomer = async (req, res) => {
   const { nome, email, cpf } = req.body;
 
   try {
-    const customer = await database('clientes').where({email}).orWhere({cpf}).debug(); 
-    
+    const customer = await database("clientes")
+      .where({ email })
+      .orWhere({ cpf })
+      .debug();
+
     return res.json(customer);
   } catch (error) {
     console.log(error.message);
-    return res.status(500).json({ mensagem: 'Erro interno do servidor.' });
+    return res.status(500).json({ mensagem: "Erro interno do servidor." });
   }
-}
+};
+
+const editCustomerData = async (req, res) => {
+  const { id } = req.params;
+  const { nome, email, cpf } = req.body;
+
+  if (!id) {
+    return res
+      .status(400)
+      .json({ mensagem: "Digite um id de cliente válido." });
+  }
+
+  try {
+    const customerExists = await database("clientes").where({ id }).first();
+
+    if (!customerExists) {
+      return res.status(404).json({ mensagem: "Cliente não encontrado." });
+    }
+
+    await database("clientes")
+      .where({ id })
+      .update({ nome: nameFormatter(nome), email, cpf: cpfFormatter(cpf) });
+
+    return res.status();
+  } catch (error) {}
+};
 
 module.exports = {
-  registerCustomer
-}
+  registerCustomer,
+  editCustomerData,
+};
