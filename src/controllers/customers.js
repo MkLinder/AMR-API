c5b80433ead07e8f68ba00def4f60875e76f2597
Mike Linder <94409465+MkLinder@users.noreply.github.com>
2023-12-02 22:56:23 -0300

    Finished controllers/editCustomerData

--- src/controllers/customers.js
+++ src/controllers/customers.js
@@ -1,20 +1,19 @@
-const database = require("../connection");
-const formatCep = require("../utils/cepFormatter");
-const { propertiesFormatter } = require("../utils/dataFormatter");
+const database = require('../connection');
+const formatCep = require('../utils/cepFormatter');
+const { propertiesFormatter } = require('../utils/dataFormatter');
 
 const registerCustomer = async (req, res) => {
   const { email, cpf, cep } = req.body;
 
   try {
-    const customer = await database("clientes")
+    const customer = await database('clientes')
       .where({ email })
-      .orWhere({ cpf })
-      .debug();
+      .orWhere({ cpf });
 
     if (customer.length > 0) {
       return res
         .status(400)
-        .json({ mensagem: "Já existe email ou cpf cadastrado." });
+        .json({ mensagem: 'Já existe email ou cpf cadastrado.' });
     }
 
     if (cep) {
@@ -27,41 +26,69 @@ const registerCustomer = async (req, res) => {
 
     propertiesFormatted.email = email;
 
-    const newCustomer = await database("clientes").insert(propertiesFormatted);
+    const newCustomer = await database('clientes').insert(propertiesFormatted);
 
     if (newCustomer.rowCount === 0) {
-      return res.status(400).json("O cliente não foi cadastrado.");
+      return res.status(400).json('O cliente não foi cadastrado.');
     }
 
-    return res.status(200).json("Cliente cadastrado com sucesso.");
+    return res.status(200).json('Cliente cadastrado com sucesso.');
   } catch (error) {
-    return res.status(500).json({ mensagem: "Erro interno do servidor." });
+    return res.status(500).json({ mensagem: 'Erro interno do servidor.' });
   }
 };
 
 const editCustomerData = async (req, res) => {
   const { id } = req.params;
-  const { nome, email, cpf } = req.body;
+  const { nome, email, cpf, cep } = req.body;
 
   if (!id) {
     return res
       .status(400)
-      .json({ mensagem: "Digite um id de cliente válido." });
+      .json({ mensagem: 'Digite um id de cliente válido.' });
   }
 
   try {
-    const customerExists = await database("clientes").where({ id }).first();
+    const customerExists = await database('clientes').where({ id }).first();
 
     if (!customerExists) {
-      return res.status(404).json({ mensagem: "Cliente não encontrado." });
+      return res.status(404).json({ mensagem: 'Cliente não encontrado.' });
     }
 
-    await database("clientes")
-      .where({ id })
-      .update({ nome: nameFormatter(nome), email, cpf: cpfFormatter(cpf) });
+    const EmailCpfExists = await database('clientes')
+      .where({ email })
+      .orWhere({ cpf });
 
-    return res.status();
-  } catch (error) {}
+    if (EmailCpfExists.length > 0) {
+      for (const item of EmailCpfExists) {
+        if (item.id !== customerExists.id) {
+          return res.status(400).json({ mensagem: 'Email ou cpf inválido.' });
+        }
+      }
+    }
+
+    if (cep) {
+      req.body.cep = formatCep(cep);
+    }
+
+    const { email: _, ...customerData } = req.body;
+
+    const propertiesFormatted = propertiesFormatter(customerData);
+
+    propertiesFormatted.email = email;
+
+    const newCustomer = await database('clientes')
+      .where({ id })
+      .update(propertiesFormatted);
+
+    if (newCustomer.rowCount === 0) {
+      return res.status(400).json('O cliente não foi cadastrado.');
+    }
+
+    return res.status(200).json('Cadastro atualizado.');
+  } catch (error) {
+    return res.status(500).json({ mensagem: 'Erro interno do servidor.' });
+  }
 };
 
 module.exports = {
