6e27a29c1d14af7d0fc2f1b85a771768ba352b59
AlvaroAzevedoDev <alvinho.azevedo@hotmail.com>
2023-12-02 21:27:14 -0300

    Created listCostumers and getCustomerDetails endpoints

--- src/controllers/customers.js
+++ src/controllers/customers.js
@@ -1,12 +1,12 @@
-const database = require("../connection");
-const formatCep = require("../utils/cepFormatter");
-const { propertiesFormatter } = require("../utils/dataFormatter");
+const database = require('../connection');
+const formatCep = require('../utils/cepFormatter');
+const { propertiesFormatter } = require('../utils/dataFormatter');
 
 const registerCustomer = async (req, res) => {
   const { email, cpf, cep } = req.body;
 
   try {
-    const customer = await database("clientes")
+    const customer = await database('clientes')
       .where({ email })
       .orWhere({ cpf })
       .debug();
@@ -14,7 +14,7 @@ const registerCustomer = async (req, res) => {
     if (customer.length > 0) {
       return res
         .status(400)
-        .json({ mensagem: "Já existe email ou cpf cadastrado." });
+        .json({ mensagem: 'Já existe email ou cpf cadastrado.' });
     }
 
     if (cep) {
@@ -27,15 +27,15 @@ const registerCustomer = async (req, res) => {
 
     propertiesFormatted.email = email;
 
-    const newCustomer = await database("clientes").insert(propertiesFormatted);
+    const newCustomer = await database('clientes').insert(propertiesFormatted);
 
     if (newCustomer.rowCount === 0) {
-      return res.status(400).json("O cliente não foi cadastrado.");
+      return res.status(400).json('O cliente não foi cadastrado.');
     }
 
-    return res.status(200).json("Cliente cadastrado com sucesso.");
+    return res.status(200).json('Cliente cadastrado com sucesso.');
   } catch (error) {
-    return res.status(500).json({ mensagem: "Erro interno do servidor." });
+    return res.status(500).json({ mensagem: 'Erro interno do servidor.' });
   }
 };
 
@@ -46,17 +46,17 @@ const editCustomerData = async (req, res) => {
   if (!id) {
     return res
       .status(400)
-      .json({ mensagem: "Digite um id de cliente válido." });
+      .json({ mensagem: 'Digite um id de cliente válido.' });
   }
 
   try {
-    const customerExists = await database("clientes").where({ id }).first();
+    const customerExists = await database('clientes').where({ id }).first();
 
     if (!customerExists) {
-      return res.status(404).json({ mensagem: "Cliente não encontrado." });
+      return res.status(404).json({ mensagem: 'Cliente não encontrado.' });
     }
 
-    await database("clientes")
+    await database('clientes')
       .where({ id })
       .update({ nome: nameFormatter(nome), email, cpf: cpfFormatter(cpf) });
 
@@ -64,7 +64,34 @@ const editCustomerData = async (req, res) => {
   } catch (error) {}
 };
 
+const listCustomers = async (req, res) => {
+  try {
+    const customers = await database('clientes');
+
+    return res.status(200).json(customers);
+  } catch (error) {
+    return res.status(500).json({ mensagem: 'Erro interno do servidor.' });
+  }
+};
+
+const getCustomerDetails = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const existingCustomer = await database('clientes').where({ id }).first();
+
+    if (!existingCustomer) {
+      return res.status(404).json({ mensagem: 'Cliente não encontrado.' });
+    }
+
+    return res.status(200).json(existingCustomer);
+  } catch (error) {
+    return res.status(500).json({ mensagem: 'Erro interno do servidor.' });
+  }
+};
 module.exports = {
   registerCustomer,
   editCustomerData,
+  listCustomers,
+  getCustomerDetails,
 };
