fe8c70b48b750d5c8add996337f83fe89fa64209
Mike Linder <94409465+MkLinder@users.noreply.github.com>
2023-12-07 18:07:23 -0300

    Created orders/listOrders

--- src/controllers/orders.js
+++ src/controllers/orders.js
@@ -18,4 +18,30 @@ const registerOrder = async (req, res) => {
   }
 };
 
-module.exports = { registerOrder };
+const listOrders = async (req, res) => {
+  const { cliente_id } = req.query;
+
+  try {
+    if (cliente_id) {
+      const customerExists = await database('clientes')
+        .where({ id: cliente_id })
+        .first();
+
+      if (!customerExists) {
+        return res.status(404).json({ mensagem: 'Cliente não encontrado.' });
+      }
+
+      return res
+        .status(200)
+        .json(await database('pedidos').where({ cliente_id }));
+    }
+
+    return res.status(200).json(await database('pedidos'));
+  } catch (error) {
+    return res.status(500).json({ mensagem: 'Erro interno do servidor.' });
+  }
+};
+module.exports = {
+  registerOrder,
+  listOrders,
+};
